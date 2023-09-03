6da207e03f4747b813304812c97363fc2203a69f
Irina Fok <fokira@mail.ru>
2023-09-03 11:28:07 +0300

    Изменение тестов

--- lists/tests.py
+++ lists/tests.py
@@ -8,18 +8,13 @@ from lists.views import home_page
 class HomePageTest(TestCase):
     """Тест домашней страницы."""
 
-    def test_root_url_resolves_to_home_page_view(self):
-        """Тест: корневой url преобразуется в представление домашней страницы."""
-        found = resolve("/")
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """Тест: домашняя страница возвращает правильный html."""
+    def test_uses_home_template(self):
+        """Тест: используется домашний шаблон."""
         response = self.client.get("/")
+        self.assertTemplateUsed(response, "home.html")
 
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.endswith("</html>"))
-
+    def test_can_save_a_POST_request(self):
+        """Тест: можно сохранить post-запрос."""
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertIn("A new list item", response.content.decode())
         self.assertTemplateUsed(response, "home.html")
