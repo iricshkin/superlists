8a22523aa54bf20460c21a6810033d57c1d9586d
Irina Fok <fokira@mail.ru>
2023-09-07 15:52:44 +0300

    Сделать functional_tests приложением, использовать LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
-import unittest
+from django.test import LiveServerTestCase
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """Тест нового посетителя."""
 
     def setUp(self) -> None:
@@ -27,7 +27,7 @@ class NewVisitorTest(unittest.TestCase):
         # Эдит слышала про крутое новое онлайн-приложение со
         # списком неотложных дел. Она решает оценить его
         # домашнюю страницу
-        self.browser.get("http://localhost:8000")
+        self.browser.get(self.live_server_url)
 
         # Она видит, что заголовок и шапка страницы говорят о
         # списках неотложных дел
@@ -69,7 +69,3 @@ class NewVisitorTest(unittest.TestCase):
         # Она посещает этот URL-адрес - ее список по-прежнему там.
 
         # Удовлетворенная, она снова ложится спать
-
-
-if __name__ == "__main__":
-    unittest.main(warnings="ignore")
