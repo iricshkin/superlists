857d588ea51bc20f41d23304bd29d56824acd297
Irina Fok <fokira@mail.ru>
2023-09-16 16:33:12 +0300

    new URL, view and template to display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,4 +4,9 @@ from lists import views
 
 urlpatterns = [
     re_path(r"^$", views.home_page, name="home"),
+    re_path(
+        r"^lists/one-of-a-kind-list-in-the-world/$",
+        views.view_list,
+        name="view_list",
+    ),
 ]
