a4357eeb7acfcb9f09be5bfb100e7b060df46884
Irina Fok <fokira@mail.ru>
2023-09-19 20:40:34 +0300

    Корректировка моделей,чтобы элементы были связаны с разными списками

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 
 
+class List(models.Model):
+    """Модель списка."""
+
+    pass
+
+
 class Item(models.Model):
     """Элемент списка."""
 
     text = models.TextField(default="")
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
