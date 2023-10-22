9658ddf9554f1952fafe6461e572ba382e657927
Irina Fok <fokira@mail.ru>
2023-10-22 15:47:11 +0300

    Обеспеена валидация модели в представлении списка

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -71,10 +71,6 @@ class ListViewTest(TestCase):
 
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
 
-
-class NewListTest(TestCase):
-    """Тесть нового списка."""
-
     def test_validation_errors_are_sent_back_to_home_page_template(self):
         """Тест: ошибки валидации отсылаются назад в шаблон домашней страницы."""
         response = self.client.post("/lists/new", data={"item_text": ""})
@@ -88,3 +84,12 @@ class NewListTest(TestCase):
         self.client.post("/lists/new", data={"item_text": ""})
         self.assertEqual(List.objects.count(), 0)
         self.assertEqual(Item.objects.count(), 0)
+
+    def test_validation_errors_end_up_on_lists_page(self):
+        """Тест: ошибки валидации оканчиваются на странице списков."""
+        list_ = List.objects.create()
+        response = self.client.post(f"/lists/{list_.id}/", data={"item_text": ""})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "list.html")
+        expected_error = escape("You can't have an empty list item")
+        self.assertContains(response, expected_error)
