6889fbb44b6f3ffe663bdcbd492b6a1b2a7b023d
Irina Fok <fokira@mail.ru>
2023-11-05 21:11:36 +0300

    Реализована валидация наличия повторяющегося элемента на уровне модели

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
-from django.core.exceptions import ValidationError
 
-from lists.models import Item, List
+from lists.models import List
 from lists.forms import ItemForm
 
 
@@ -18,7 +17,7 @@ def view_list(request, list_id):
     if request.method == "POST":
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST["text"], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
     return render(request, "list.html", {"list": list_, "form": form})
 
@@ -28,7 +27,7 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST["text"], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, "home.html", {"form": form})
