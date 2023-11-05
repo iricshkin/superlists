a72038fd4c919195b3995d89228243109d58a04a
Irina Fok <fokira@mail.ru>
2023-11-05 22:54:19 +0300

    Изменена форма для элемента списка

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -56,3 +56,10 @@ class ExistingListItemFormTest(TestCase):
         form = ExistingListItemForm(for_list=list_, data={"text": "no twins!"})
         self.assertFalse(form.is_valid())
         self.assertEqual(form.errors["text"], [DUPLICATE_ITEM_ERROR])
+
+    def test_form_save(self):
+        """Тест сохранения формы."""
+        list_ = List.objects.create()
+        form = ExistingListItemForm(for_list=list_, data={"text": "hi"})
+        new_item = form.save()
+        self.assertEqual(new_item, Item.objects.all()[0])
