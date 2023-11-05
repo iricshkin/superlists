53eecd96d99e75c2527cf692fd752900cf6bf4a0
Irina Fok <fokira@mail.ru>
2023-11-05 21:47:36 +0300

    Быстрая фиксация

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,6 +3,7 @@ from django.utils.html import escape
 
 from lists.models import Item, List
 from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from unittest import skip
 
 
 class HomePageTest(TestCase):
@@ -109,6 +110,19 @@ class ListViewTest(TestCase):
         self.assertIsInstance(response.context["form"], ItemForm)
         self.assertContains(response, 'name="text"')
 
+    @skip
+    def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
+        """Тест: ошибки валидации повторяющегося элемента
+        оканчиваются на странице списков."""
+        list1 = List.objects.create()
+        item1 = Item.objects.create(list=list1, text="textey")
+        response = self.client.post(f"/lists/{list1.id}", data={"text": "textey"})
+
+        expected_error = escape("You've already got this in your list")
+        self.assertContains(response, expected_error)
+        self.assertTemplateUsed(response, "list.html")
+        self.assertEqual(Item.objects.all().count(), 1)
+
 
 class NewListTest(TestCase):
     """Тест нового списка."""
@@ -130,7 +144,7 @@ class NewListTest(TestCase):
         response = self.client.post("/lists/new", data={"text": ""})
         self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-    def test_for_invalid_input_passes_form_to__template(self):
+    def test_for_invalid_input_passes_form_to_template(self):
         """Тест на недопустимый ввод: форма передается в шаблон."""
         response = self.client.post("/lists/new", data={"text": ""})
         self.assertEqual(response.status_code, 200)
